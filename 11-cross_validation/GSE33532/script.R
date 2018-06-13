077b7e96868f9c57114eafd074656910dd1eca1c
Elena Papaleo <ele@Elenas-MacBook-Pro.local>
2018-06-13 22:31:42 +0200

    added cross-validation

---
+++ 11-cross_validation/GSE33532/script.R
@@ -0,0 +1,60 @@
+library(gProfileR)
+library(WGCNA)
+
+data <- read.csv("final_matrix.csv",row.names = 1)
+rowID <- rownames(data)
+dim(data)
+
+conversion <- gconvert(rowID, organism = "hsapiens", target="AFFY_HG_U133_PLUS_2")
+write.csv <- write.csv(conversion,file = "conversion_file.csv")
+conversion <- read.csv("conversion_file.csv",row.names = 1)
+
+gene_symbol <- conversion$name[match(rowID,tolower(conversion$alias))]
+length(gene_symbol)
+# remove all the non-converted probes
+conversion_data <- na.omit(cbind(rowID,as.character(gene_symbol)))
+new_data <- subset(data,rownames(data) %in% conversion_data[,"rowID"])
+dim(new_data)
+
+rowGroup <- conversion_data[,2]
+rowID <- rownames(new_data)
+
+# collapse
+collapse.object=collapseRows(datET=new_data, rowGroup=rowGroup, rowID=rowID)
+
+dat1Collapsed=data.frame(collapse.object$group2row, collapse.object$datETcollapsed)
+# check if the gene names are unique
+nrow(dat1Collapsed)
+length(unique(dat1Collapsed$group))
+length(unique(dat1Collapsed$selectedRowID))
+# remove group and selectedRowID columns
+dat1Collapsed <- dat1Collapsed[,- c(1,2)]
+write.csv(dat1Collapsed,file="collapsed_data.csv")
+
+
+#########################################################################
+
+dat1Collapsed <- read.csv("collapsed_data.csv",row.names = 1)
+# retain only the genes of interest
+gene_list <- c("MUC5","BHABP2","MUC21","KCNK5","ICA1","CSTA","P2RY1","ANXA8","FZD7","ITGA6",
+               "CHST7","RND3","ACOX2","ALDOC","AQP5","ARSE","FABP5","SIPA1L2","SLC1A3","SLC2A9")
+
+dat1Collapsed <- dat1Collapsed[which(rownames(dat1Collapsed) %in% gene_list),]
+
+library(gplots)
+mydist=function(c) {dist(c,method="euclidean")}
+myclust=function(c) {hclust(c,method="complete")}
+# plot heatmap
+clab <- rep("magenta",ncol(dat1Collapsed))
+clab[grep("LUSC",colnames(dat1Collapsed))] <- "cyan"
+
+pdf("heatmap_cross_validation.pdf")
+
+heatmap.2(as.matrix(dat1Collapsed), distfun = mydist, hclustfun = myclust,
+          ColSideColors = clab, trace = "none", density.info="none",
+          col= colorRampPalette(c("white", "blue"))( 150 ), margins =c(12,9))
+par(lend = 1) 
+legend("topright", legend = c("LUAD","LUSC"), col = c("magenta","cyan"),
+       lty = 1,lwd = 10, border = FALSE, bty="n", y.intersp = 0.7, cex=0.9)
+
+dev.off()
